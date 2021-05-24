26e3f372221188a3d26da06ffd279c881330538f
Eliza Huang <eliza.huang@alaskaair.com>
2021-05-24 16:02:43 -0700

    feat: auro-banner contract

--- src/auro-banner.js
+++ src/auro-banner.js
@@ -7,68 +7,41 @@
 // ---------------------------------------------------------------------
 
 import { LitElement, html, css } from "lit-element";
-import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { classMap } from 'lit-html/directives/class-map';
+import { styleMap } from 'lit-html/directives/style-map';
+import styleCss from "./style-banner-css.js";
 
 // Import touch detection lib
 import "focus-visible/dist/focus-visible.min.js";
-import styleCss from "./style-banner-css.js";
 
 import "@alaskaairux/auro-header";
 
 // See https://git.io/JJ6SJ for "How to document your components using JSDoc"
 /**
  * The auro-card-hero element provides users a flexible way to convey a summary of information in various large formats.
- * @attr {Boolean} billboard - to be used for billboard style configuration
- * @attr {Boolean} hero - to be used for hero style configuration
- * @attr {Boolean} iconic - to be used in as a hero on pages but with an icon and no displayImage on mobile
- * @attr {String} iconbg - to be used in conjunction with the iconic variant this specifies the background color of the icon
- * @attr {Boolean} marquee - to be used for marquee style configuration
- * @attr {Boolean} roundedBorder - to be used for roundedBorder style configuration
- * @attr {Boolean} solid - to be used when you want a solid color as opposed to a transparent background
- * @attr {Boolean} slim - to be used when we want a slimmer padding to the default banner
- * @attr {Boolean} alignRight - to be used when we want the text aligned to the right
- * @attr {Boolean} alignLeft - to be used when we want the text aligned to the left
- * @attr {Boolean} onDark - to be used when the background image or color is dark and changes the text and cta color
- * @slot displayImage - placement for `<picture />` or `<img>` elements
- * @slot prefix - placement for smaller text above title
- * @slot title - placement for header
- * @slot contentImage - image placement
- * @slot description - main body of content
- * @slot action - call to action
- * @slot disclaimer - disclaimer copy
+ * @attr {Boolean} prefersLeft - 
+ * @attr {Boolean} prefersRight - 
+ * @attr {String} ratio - 
+ * @slot left - 
+ * @slot right - 
+ * @slot overlay - 
  */
 class AuroBanner extends LitElement {
 
-
-    // This function removes a CSS selector if the footer slot is empty
-    firstUpdated() {
-      const slotNodes = this.shadowRoot.querySelectorAll(`.bannerWrapper slot`);
-
-      for (const item of slotNodes) {
-        this.slt = item.assignedNodes();
-        // eslint-disable-next-line no-magic-numbers
-        if (this.slt.length === 0) {
-          item.removeAttribute("class");
-        }
-      }
-    }
+  constructor() {
+    super();
+    this.ratio = '1:1';
+  }
 
   static get properties() {
     return {
-      ...super.properties,
-      hero: {
+      perfersLeft: {
         type: Boolean
       },
-      iconic: {
+      prefersRight: {
         type: Boolean
       },
-      marquee: {
-        type: Boolean
-      },
-      roundedBorder: {
-        type: Boolean
-      },
-      iconbg: {
+      ratio: {
         type: String
       }
     };
@@ -82,76 +55,25 @@ class AuroBanner extends LitElement {
 
   // function that renders the HTML and CSS into  the scope of the component
   render() {
+    const leftRatio, rightRatio = this.ratio.split(':');
+
+    const classes = {
+      'prefersRight': this.prefersRight,
+      'prefersLeft': this.prefersLeft && !this.prefersRight,
+    };
+    const leftSlotStyles = {
+      'flex-grow': leftRatio,
+    };
+    const rightSlotStyles = {
+      'flex-grow': rightRatio,
+    };
+
     return html`
-      <div class="bannerWrapper">
-      ${!this.roundedBorder
-        ? html`
-        <slot name="displayImage" class="displayImage"></slot>`
-        : html``
-      }
-        <div class="bodyWrapper">
-          ${this.iconic
-           ? html`
-             <slot name="title"></slot>`
-            : html``
-          }
-          ${this.hero
-            ? html`
-              <auro-header level="2" display="300" margin="top" size="none" class="title prefix">
-                <slot name="prefix"></slot>
-              </auro-header>
-
-              <auro-header level="2" display="600" margin="both" size="none" class="title">
-                <slot name="title"></slot>
-              </auro-header>`
-            : html``
-          }
-
-          ${this.marquee
-            ? html`
-              <auro-header level="2" display="400" margin="both" size="none" class="title marquee">
-                <slot name="title"></slot>
-              </auro-header>`
-            : html``
-          }
-
-          ${!this.marquee && !this.hero && !this.iconic && !this.roundedBorder
-            ? html`
-              <auro-header level="2" display="600" margin="both" size="none" class="title">
-                <slot name="title"></slot>
-              </auro-header>`
-            : html``
-          }
-
-          <slot name="contentImage" class="imageWrapper"></slot>
-
-          <div class="contentWrapper">
-          ${this.roundedBorder
-            ? html`
-              <auro-header level="2" display="300" margin="both" size="none" class="title prefix">
-                <slot name="prefix"></slot>
-              </auro-header>
-
-              <auro-header level="2" display="600" margin="both" size="none" class="title">
-                <slot name="title"></slot>
-              </auro-header>`
-            : html``
-          }
-            <slot name="description" class="description"></slot>
-            <slot name="action" class="action"></slot>
-            <slot name="disclaimer" class="disclaimer"></slot>
-          </div>
-        </div>
-      </div>
-      ${this.iconic
-        ? html`
-        <div class="iconContainer">
-          <div class="roundIconBg" style="background: ${ifDefined(!this.iconbg ? undefined : this.iconbg)}">
-            <slot name="icon" class="icon"></slot>
-          </div>
-        </div>`
-        : html``
-      }
+    <div class="bannerWrapper" class="${classMap(classes)}">
+      <slot name="left" style=${styleMap(leftSlotStyles)}></slot>
+      <slot name="right" style=${styleMap(rightSlotStyles)}></slot>
+      <slot name="overlay"></slot>
+    </div>
     `;
   }
 }
