7bd86e0df786263cc4708c4ffed7cb965ecb56ef
Eliza Huang <eliza.huang@alaskaair.com>
2021-05-27 14:33:02 -0700

    auro-card tests

--- test/auro-card.test.js
+++ test/auro-card.test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef, sort-vars, no-implicit-coercion */
 
 import { fixture, html, expect } from '@open-wc/testing';
-// import sinon from 'sinon';
 import '../src/auro-card.js';
 
 describe('auro-card', () => {
@@ -10,27 +9,23 @@ describe('auro-card', () => {
     const el = await fixture(html`
       <auro-card>
         <span slot="title">
-          <b>one</b>world Explorer
+          Pri dicat graece abhorreant
         </span>
         <img
-          slot="image"
-          src="https://sitecore-test-single-westus2.azurewebsites.net/-/media/Images/pages/examples/blue.png"
-          alt="This is an image of a logo" />
-        <p slot="description">
-          <b>one</b>world Explorer fare makes it easy to travel from city to city, and continent to continent. And, for every dot you connect, you earn more miles and points to spend across the <b>one</b>world Alliance.
-        </p>
-        <auro-hyperlink
-          href="/"
-          slot="cta"
-          target="_blank">
-          More info
-        </auro-hyperlink>
-        <p slot="disclaimer">
-          * Void where prohibited.
-        </p>
+          slot="top"
+          src="https://picsum.photos/304?random=1"
+          alt="Random insert 0"/>
+        <div slot="bottom">
+          <p>
+            Ei est detracto fabellas convenire, vivendo accusata intellegebat ex mei.
+          </p>
+          <p>
+            * Errem tempor eligendi cu eos
+          </p>
+        </div>
       </auro-card>
     `);
-
+    await expect(el).to.not.be.null;
     await expect(el).to.be.accessible();
   });
 
@@ -38,5 +33,27 @@ describe('auro-card', () => {
     const el = await !!customElements.get("auro-card");
 
     await expect(el).to.be.true;
+  });
+
+  it('auro-card can have set widths and heights', async () => {
+    const el = await fixture(html`
+      <auro-card width="19rem" height="31rem">
+        <img
+          slot="top"
+          src="https://picsum.photos/304?random=1"
+          alt="Random insert 0"/>
+        <div slot="bottom">
+          <p>
+            Ei est detracto fabellas convenire, vivendo accusata intellegebat ex mei.
+          </p>
+          <p>
+            * Errem tempor eligendi cu eos
+          </p>
+        </div>
+      </auro-card>
+    `);
+    await expect(el).to.not.be.null;
+    await expect(el.getAttribute('width')).to.equal('19rem');
+    await expect(el.getAttribute('height')).to.equal('31rem');
   });
 });
